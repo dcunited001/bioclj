d4a48cfca961f73e59639d850fcefdfaff80510d
David Conner <dconner.pro@gmail.com>
2014-11-02 00:02:33 -0400

    * implemented and tested the frequent kmer function

--- src/bioclj/core.clj
+++ src/bioclj/core.clj
@@ -1,12 +1,22 @@
 (ns bioclj.core
-   (:require [clojure.math.combinatorics :as com]))
+  (:require [clojure.math.combinatorics :as com]))
 
 (defn combinate-domain
   "Permutes a string or array of chars and returns an array of strings representing the domain"
   [chars]
   ;; the algorithm this way is very inefficient.  hash is always most efficient.
   ;; plus, clojure already has the com/permutate function
-  (com/combinations chars (count chars)))
+  (com/selections chars (count chars)))
 
-(defn count-str [str in])
-(defn iterate-str [in fn])
+(defn frequent-kmer
+  "returns a hash where keys are kmers and values are frequency.  accepts k and text."
+  [k text]
+  (let [idxstop (- (count text) k)]
+    (persistent!
+      (reduce
+        #(let [kmer (subs text %2 (+ %2 k))
+               cnt (%1 kmer 0)]
+          (assoc! %1 kmer (inc cnt)))
+        (transient {})
+        (range 0 (inc idxstop))
+        ))))
--- test/bioclj/t_core.clj
+++ test/bioclj/t_core.clj
@@ -3,17 +3,21 @@
   (:require [bioclj.core :as core]))
 
 (facts permute-domain
-  (fact "Permuting strings works"
-    (let [permuted (core/combinate-domain "AC")]
-      permuted
-      ))
+       (fact "Permuting strings works"
+             (core/combinate-domain "AC") => '((\A \A) (\A \C) (\C \A) (\C \C)))
 
-  (fact "Permuting arrays works"
-    (let [permuted (core/combinate-domain [\A \B])]
-      permuted
-      ))
+       (fact "Permuting arrays works"
+             (core/combinate-domain [\A \B]) => '((\A \A) (\A \B) (\B \A) (\B \B))))
 
-  (fact "Permuting ACTG works"
-    (let [permuted (core/combinate-domain "ACTG")]
-      permuted
-      )))
+(facts freqent-kmer
+       (fact "counting kmers works for a 1-mers"
+             (let [freq (core/frequent-kmer 1 "ABCDCD")]
+               (freq "D") => 2
+               (freq "C") => 2
+               (freq "A") => 1))
+
+       (fact "counting kmers works for a 2-mers"
+             (let [freq (core/frequent-kmer 2 "ABCDCD")]
+               (freq "DC") => 1
+               (freq "CD") => 2
+               (freq "AB") => 1)))
