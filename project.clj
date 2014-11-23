098ff6bf1eee405ee8fc4a4b000d0a0a9ebe808f
David Conner <dconner.pro@gmail.com>
2014-11-22 19:07:06 -0500

    * set gloss to the correct version...

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [org.clojure/math.combinatorics "0.0.8"]
                  [clj-biosequence "0.1.4-SNAPSHOT"]
                  [criterium "0.4.3"]
-                 [gloss "2.0.3"]
+                 [gloss "0.2.3"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
   :main bioclj.core)
