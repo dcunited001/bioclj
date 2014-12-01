e6c345317082e56eee68728f12ea3377392bda40
David Conner <dconner.pro@gmail.com>
2014-11-30 22:09:03 -0500

    * implemented median-string distance functions
    * implemented motif-profile-most-probable-kmer, but completely wrong....

--- src/bioclj/motif.clj
+++ src/bioclj/motif.clj
@@ -66,17 +66,82 @@
 ;; and there may be a way to utilize the change in neighborhood set from the base neighborhood, a ∂-N(kmer,d) function,
 ;;   to simplify the disjunction of neighborhoods
 
-(defn score-motif
+
+(defn median-string-max-distance
+  [k dna]
+  (* (count dna) k))
+
+(defn median-string-distance
+  [dna-seqs pattern]
+  (let [k (:k (first dna-seqs))]
+    (r/fold
+      4
+      (fn foldsum ([] 0) ([x y] (+ x y)))
+      (fn sum-minimums [total dna-seq]
+        (+ total
+           (reduce
+             #(let [dist (hamming-64b k pattern %2)]
+               (if (< dist %1)
+                 dist
+                 %1))
+             Double/POSITIVE_INFINITY
+             (:b64 dna-seq))))
+      dna-seqs)))
+
+
+;; instead of using the set of all kmers as a starting domain
+;; may be possible to use the disjunction of seq-neighbors for all seqs in dna
+;; as a starting domain instead
+(defn median-string
+  [k dna]
+  (let [dna-seqs (if (string? (first dna))
+                   (map (partial acgt-get-64b-kmers k) dna)
+                   dna)]
+    (reduce
+      #(let [shifted-pattern (bit-shift-left %2 (- 64 (* 2 k)))
+             dist (median-string-distance dna-seqs shifted-pattern)]
+        (if (< dist (:min %1))
+          {:median shifted-pattern :min dist}
+          %1))
+      {:median -1 :min Double/POSITIVE_INFINITY}
+      (range 0 (maths/expt 4 k)))))
+
+(defn motif-count
   [motifs]
 
   )
 
-(defn count-motif
+(defn motif-profile
   [motifs]
 
   )
 
-(defn profile-motif
-  [motifs]
+;;TODO: profile record and methods for operating on them
+(defn motif-profile-most-probable-kmer
+  [dna k profile]
+  (reduce
+    (fn [nucs pos]
+      (conj nucs
+            (reduce
+              (fn [nuc i]
+                (let [new-max (nth profile (+ (* k i) pos))]
+                  (if (> new-max (:max nuc))
+                    {:max new-max :nuc [i]}
+                    (if (= new-max (:max nuc))
+                      (assoc nuc :nuc (conj (:nuc nuc) i))
+                      nuc))))
+              {:max -1 :nuc []}
+              (range 0 4))))
+    []
+    (range k)))
 
-  )
+(defn motif-most-probable-to-kmers
+  "converts the array of hashed returned above to a nucleotide"
+  [dna mpk]
+  ;;TODO: alter this to return the first nucleotides in dna
+  (reduce
+    #(let [m (nth mpk %2)
+           nucleo (nth (acgt-64b-shifted-nucleotides %2) (first (:nuc m)))]
+      (bit-or %1 nucleo))
+    0
+    (range (count mpk))))
